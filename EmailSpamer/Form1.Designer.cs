4c6ab19b564f608537d82a1851a8a09da8493e93
HackSh00t <anonymousmegamaster@gmail.com>
2018-12-27 14:36:08 +0100

    Installer Created

--- EmailSpamer/Form1.Designer.cs
+++ EmailSpamer/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.IbkEmail = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -282,6 +283,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.IbkEmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "EmailSpamer";
             ((System.ComponentModel.ISupportInitialize)(this.spam)).EndInit();
